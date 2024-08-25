647a5984bcb73ccf321782ed29b9eb37543c63e3
howdy10 <howdy1078@gmail.com>
2024-08-25 15:07:31 -0700

    Updated github pages files

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,17 @@ const config: Config = {
   favicon: 'img/icon.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://howdy10.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'howdy10', // Usually your GitHub org/user name.
+  projectName: 'howdy10.github.io', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
