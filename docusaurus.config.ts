913181621f62de9b8a743951c12448a916f92e36
howdy10 <howdy1078@gmail.com>
2024-08-29 22:41:17 -0700

    Added inspect easter egg

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,6 +56,44 @@ const config: Config = {
     ],
   ],
 
+  ssrTemplate: `<!DOCTYPE html>
+<html <%~ it.htmlAttributes %>>
+<!--
+    If you are seeing this let me save you the trouble
+
+    -I did not built this from scratch
+    -I started with the base of docusaurus
+    -I am still using the base css it came with
+    -The writing are using the markdown pages
+    -It is using React so I did built so base react things
+
+    Thank you for coming to my ted talk -Esaul
+-->
+  <head>
+    <meta charset="UTF-8">
+    <meta name="generator" content="Docusaurus v<%= it.version %>">
+    <% it.metaAttributes.forEach((metaAttribute) => { %>
+      <%~ metaAttribute %>
+    <% }); %>
+    <%~ it.headTags %>
+    <% it.stylesheets.forEach((stylesheet) => { %>
+      <link rel="stylesheet" href="<%= it.baseUrl %><%= stylesheet %>" />
+    <% }); %>
+    <% it.scripts.forEach((script) => { %>
+      <link rel="preload" href="<%= it.baseUrl %><%= script %>" as="script">
+    <% }); %>
+  </head>
+  <body <%~ it.bodyAttributes %>>
+    <%~ it.preBodyTags %>
+    <div id="__docusaurus">
+      <%~ it.appHtml %>
+    </div>
+    <% it.scripts.forEach((script) => { %>
+      <script src="<%= it.baseUrl %><%= script %>"></script>
+    <% }); %>
+    <%~ it.postBodyTags %>
+  </body>
+</html>`,
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
